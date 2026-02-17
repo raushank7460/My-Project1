e0088f3009b3fd1f7be3f667deafe3c035e9b6ab
raushankumarcs23-ops <raushan.kumar_cs23@gla.ac.in>
2026-02-18 00:48:19 +0530

    update

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,49 @@
-const express=require("express");
-const app=express();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
 
-const port=4000;
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
-app.listen(port,()=>{
-    console.log("server is running ");
-})
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.get("/username",(req,res)=>{
-    res.render("data is sucessfully sent .");
-})
+async function main() {
+  await mongoose.connect(MONGO_URL);
+};
+app.get("/", (req, res) => {
+  res.send("Hi, I am root");
+});
+ewrtyu
+
+app.get("/listings",(req,res)=>{
+    Listing.find({}).then((res)=>{
+        console.log(res);
+    });
+
+});
+// app.get("/testListing", async (req, res) => {
+//   let sampleListing = new Listing({
+//     title: "My New Villa",
+//     description: "By the beach",
+//     price: 1200,
+//     location: "Calangute, Goa",
+//     country: "India",
+//   });
+
+//   await sampleListing.save();
+//   console.log("sample was saved");
+//   res.send("successful testing");
+// });
+
+
+
+
+app.listen(8080, () => {
+  console.log("server is listening to port 8080");
+});
